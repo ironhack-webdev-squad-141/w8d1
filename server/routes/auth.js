81aab4c6342aac3f194c754f19fd59b2e8b194ff
mjarraya <mjarraya@student.42.fr>
2019-07-22 11:56:16 +0200

    signup route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,37 @@ const bcrypt = require('bcrypt')
 const User = require('../models/User')
 
 router.post('/signup', (req, res, next) => {
-    // implement signup
+    const { username, password } = req.body;
+
+    if (!password || !username) {
+        return res.status(400).json({ message: "Both fields are required" });
+    } else if (password.length < 8) {
+        return res.status(400).json({ message: "Password needs to be 8 char. min" });
+    }
+
+    User.findOne({ username: username }).then(user => {
+        if (user) {
+            return res.status(409).json({ message: "Username is already taken" })
+        }
+
+        const salt = bcrypt.genSaltSync();
+        const hash = bcrypt.hashSync(password, salt);
+
+        User.create({ 
+            username,
+            password: hash
+         }).then(newUser => {
+             req.login(newUser, (err) => {
+                 if (err) {
+                     return res.json({ message: "Error while attempting to login" })
+                 }
+
+                res.status(200).json(newUser);
+             })
+         })
+
+    })
+
 })
 
 module.exports = router;
